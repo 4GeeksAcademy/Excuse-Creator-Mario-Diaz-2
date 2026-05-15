12d1614a922132e3fb37584a0cc84a9cacb36918
MarioDiaz9 <pindadodiazmario@gmail.com>
2026-05-15 09:58:23 +0000

    Optimización

--- src/app.js
+++ src/app.js
@@ -5,14 +5,14 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ['My grandma', 'My dog', 'My turtle', 'My bird', 'My cat', 'My fish', 'My hamster']
+let what = ['ate my homework', 'broke my leg', 'stole my phone', 'peed my clothes', 'stole my car keys']
+let when = ['yesterday', 'before the class', 'this morning', 'this night']
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)]
+}
+
 window.onload = function () {
-  let who = ['My grandma', 'My dog', 'My turtle', 'My bird', 'My cat', 'My fish', 'My hamster']
-  let what = ['ate my homework', 'broke my leg', 'stole my phone', 'peed my clothes', 'stole my car keys']
-  let when = ['yesterday', 'before the class', 'this morning', 'this night']
-  
-  let randomWho = Math.floor(Math.random() * who.length)
-  let randomWhat = Math.floor(Math.random() * what.length)
-  let randomWhen = Math.floor(Math.random() * when.length)
-  let completeExcuse = who[randomWho] + ' ' + what[randomWhat] + ' ' + when[randomWhen] + '.'
-  document.getElementById('excuse').innerHTML = completeExcuse
+  document.getElementById("excuse").innerHTML = getRandomElement(who) + ' ' + getRandomElement(what) + ' ' + getRandomElement(when) + '.'
 }
